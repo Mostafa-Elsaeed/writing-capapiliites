0adcce3fea3e0e10255c039dfb2a9b9cc4fe388d
Mostafa Elsaeed <98340604+Mostafa-Elsaeed@users.noreply.github.com>
2024-07-18 03:54:18 +0300

    Init DotNet App And Setup env Work Done: - init my learning dotnet app

---
+++ Models/stock.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zrustra_features.Models
+{
+    public class Stock
+    {
+
+    }
+}
